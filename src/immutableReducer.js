5c922c65df70a20dad82ad53141f55d22fd932e8
Matti Korttila <matti.korttila@gmail.com>
2016-02-28 13:34:28 +0200

    implement immutableReducer

--- src/immutableReducer.js
+++ src/immutableReducer.js
@@ -1,5 +1,127 @@
-/*eslint-disable no-unused-vars*/
-export default (actions, itemsProperty, initialState, wrappedReducer, properties) => {
-  throw new Error('immutable format not supported yet')
+import Immutable, { List } from 'immutable'
+
+function initializeProperty() {
+  return Immutable.Set()
 }
-/*eslint-enable no-unused-vars*/
+
+function createGuarantees(properties) {
+  return (state) => {
+    return state.map(list => {
+      if (!Immutable.Map.isMap(list)) {
+        throw new Error('list objects must be Immutable.Map')
+      }
+
+      properties.forEach(property => {
+        if (!list.get(property)) {
+          list = list.set(property, initializeProperty())
+        }
+      })
+
+      return list
+    })
+  }
+}
+
+export default (actions, itemsProperty, initialState = List(), wrappedReducer, properties) => {
+  const guaranteeProperties = createGuarantees(properties)
+
+  if (!List.isList(initialState)) {
+    throw new Error('initialState must be Immutable.List')
+  }
+
+  function reducer(state = guaranteeProperties(initialState), action) {
+    switch (action.type) {
+      case actions.PUSH:
+        return state.map(list => list === action.list
+          ? list.set(itemsProperty, list.get(itemsProperty).push(action.item))
+          : list
+        )
+      case actions.DELETE:
+        return state.map(list =>
+          list.set(itemsProperty,
+                   list.get(itemsProperty).filter(item => item !== action.item))
+        )
+      case actions.UPDATE: // TODO test
+        return state.map(list =>
+          list.set(itemsProperty,
+                   list.get(itemsProperty).map(item => item === action.item ? action.newItem : item))
+        )
+      case actions.MOVE:
+        return _move(state, action)
+      case actions.MOVE_TO_LIST:
+        return _moveToList(state, action)
+      case actions.TOGGLE_PROPERTY:
+        return _toggleProperty(state, action)
+      default: {
+        const newState = typeof wrappedReducer === 'function' ?
+          wrappedReducer(state, action) : state
+
+        if (!List.isList(newState)) {
+          throw new Error('state must be Immutable.List')
+        }
+
+        return guaranteeProperties(newState)
+      }
+    }
+  }
+
+  function _move(state, action) {
+    if (action.item === action.toItem) {
+      return state
+    }
+
+    /*eslint-disable indent*/
+    return state.map(list =>
+      list.set(itemsProperty,
+               list.get(itemsProperty).flatMap(item => {
+        if (item === action.item) {
+          return List()
+        }
+
+        if (item === action.toItem) {
+          return action.before ? List([action.item, item]) : List([item, action.item])
+        }
+
+        return List([item])
+      })))
+    /*eslint-enable indent*/
+  }
+
+  function _moveToList(state, action) {
+    return state.map(list => {
+      const found = list.get(itemsProperty).find(item => item === action.item)
+      const isMoveTarget = list === action.list
+
+      // cannot move item to its own list
+      if (found && isMoveTarget) {
+        return list
+      }
+
+      if (found) {
+        return list.set(itemsProperty,
+                        list.get(itemsProperty).filter(item => item !== action.item)
+        )
+      }
+
+      if (isMoveTarget) {
+        return list.set(itemsProperty, list.get(itemsProperty).push(action.item))
+      }
+
+      return list
+    })
+  }
+
+  function _toggleProperty(state, action) {
+    return state.map(list => {
+      const found = list.get(itemsProperty).find(item => item === action.item)
+      if (!found) return list
+
+      const property = list.get(action.property)
+
+      const newValue = property.has(action.item) ? property.delete(action.item) : property.add(action.item)
+      return list.set(action.property, newValue)
+    })
+  }
+
+  return reducer
+}
--- test/immutable.spec.js
+++ test/immutable.spec.js
@@ -1,12 +1,94 @@
+import Immutable from 'immutable'
 import { expect } from 'chai'
 import listreducer from '../src'
 
 describe('listreducer (immutable)', () => {
-  it('is not yet supported', () => {
-    const createReducer = () => listreducer({
-      format: 'immutable',
-      itemsProperty: 'items'
-    })
-    expect(createReducer).to.throw(/immutable format not supported/)
+
+  // This is the initialState without the `selected` property
+  // An Immutable.Set object gets set for each property
+  // when the reducer is called with any action not handle by listreducer
+  const rawLists = Immutable.fromJS([
+    {
+      name: 'foo',
+      items: [
+        2,
+        3,
+      ]
+    },
+    {
+      name: 'bar',
+      items: [
+        4,
+        5,
+      ]
+    },
+    {
+      name: 'baz',
+      items: []
+    },
+  ])
+
+  const {reducer, actionCreators} = listreducer({
+    key: 'reducer',
+    format: 'immutable',
+    itemsProperty: 'items',
+    properties: ['selected']
   })
+
+  const lists = reducer(rawLists, {type: '@@@@'})
+
+  it('returns state when action is unknown', () => {
+    expect(reducer(lists, {type: '@@@@'})).to.eql(lists)
+  })
+
+  it('can push an item', () => {
+    const action = actionCreators.push(5, lists.get(1))
+    const state = reducer(lists, action)
+    expect(state.get(1).get('items').get(2)).to.eql(5)
+  })
+
+  it('can delete an item', () => {
+    const action = actionCreators.del(2)
+    const state = reducer(lists, action)
+    expect(state.get(0).get('items').count()).to.equal(1)
+    expect(state.get(1).get('items').count()).to.equal(2)
+    expect(state.get(0).get('items').get(0)).to.equal(3)
+  })
+
+  it('can move an item before another item', () => {
+    const action = actionCreators.move(2, 4)
+    const state = reducer(lists, action)
+    expect(state.get(0).get('items').count()).to.equal(1)
+    expect(state.get(1).get('items').count()).to.equal(3)
+    // expect(state[1].items).to.eql([2, 4, 5])
+  })
+
+  it('returns state if item is moved to itself', () => {
+    const action = actionCreators.move(2, 2)
+    const state = reducer(lists, action)
+    expect(state).to.eql(lists)
+  })
+
+  it('can move an item to another list', () => {
+    const action = actionCreators.moveToList(3, lists.get(2))
+    const state = reducer(lists, action)
+    expect(state.get(0).get('items').count()).to.equal(1)
+    expect(state.get(1).get('items').count()).to.equal(2)
+    expect(state.get(2).get('items').count()).to.equal(1)
+    expect(state.get(2).get('items').get(0)).to.eql(3)
+  })
+
+  it('sets property to false by default', () => {
+    const state = reducer(lists, '@@@@')
+    expect(state.get(0).get('selected').has(3)).to.equal(false)
+  })
+
+  it('can toggle property', () => {
+    const action = actionCreators.toggleProperty('selected', 3)
+    const state = reducer(lists, action)
+    expect(state.get(0).get('selected').has(3)).to.equal(true)
+    const newState = reducer(state, action)
+    expect(newState.get(0).get('selected').has(3)).to.equal(false)
+  })
+
 })
